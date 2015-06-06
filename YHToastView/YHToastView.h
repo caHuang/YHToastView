144aa522bb4f5173e52ec7750e6cc0bf14ceacaa
Yolk Huang <yolk.tw@gmail.com>
2015-06-06 23:11:07 +0800

    Add YHToastView file

---
+++ YHToastView/YHToastView.h
@@ -0,0 +1,13 @@
+//
+//  YHToastView.h
+//  YHToastView
+//
+//  Created by Andy Huang on 6/6/15.
+//  Copyright (c) 2015 lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHToastView : UIView
+
+@end
---
+++ YHToastView/YHToastView.m
@@ -0,0 +1,13 @@
+//
+//  YHToastView.m
+//  YHToastView
+//
+//  Created by Andy Huang on 6/6/15.
+//  Copyright (c) 2015 lab. All rights reserved.
+//
+
+#import "YHToastView.h"
+
+@implementation YHToastView
+
+@end
